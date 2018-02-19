ef063b49eb64ef9bf99f469589d2b83fbcfdddf7
Bryce Klinker <bryceklinker@Bryce-Klinker.local>
2018-02-19 13:48:53 -0600

    Started adding very simple features

---
+++ Swifty/SwiftyTests/SwiftyTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import Swifty
+
+class SwiftyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+    
+    func testPerformanceExample() {
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
---
+++ Swifty/SwiftyUITests/SwiftyUITests.swift
@@ -0,0 +1,20 @@
+import XCTest
+
+class SwiftyUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCUIApplication.
+    }
+    
+}
