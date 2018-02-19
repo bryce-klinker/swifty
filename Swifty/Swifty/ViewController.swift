f413d9a5c82980f7f9a5b9ed985ef0142fac5b99
Bryce Klinker <bklinker@pillartechnology.com>
2018-02-19 14:18:57 -0600

    Added the ability to change the text using text input

--- Swifty/Swifty/ViewController.swift
+++ Swifty/Swifty/ViewController.swift
@@ -1,18 +1,27 @@
 import UIKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Properties
     @IBOutlet weak var mealNameLabel: UILabel!
     @IBOutlet weak var nameTextField: UITextField!
     
     override func viewDidLoad() {
-
-    super.viewDidLoad()
-        
+        super.viewDidLoad()
+        nameTextField.delegate = self
     }
 
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
     // MARK: Actions
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
